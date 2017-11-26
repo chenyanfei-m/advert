740cacaf18b861feb2e51362c84c08421602b21a
妫乙 <1044927473@qq.com>
2017-11-26 23:59:19 +0800

    修改图片

--- app.js
+++ app.js
@@ -8,37 +8,39 @@ let app = express()
 app.use(express.static('static'))
 
 const equipmentTypeDetection = (ua) => {
-  const typeArr = ['iphone', 'android', 'ipad', 'windows', 'macos']
-  const findResult = typeArr.find(ele => ua.match(new RegExp(ele, 'i')))
-  return findResult || 'windows'
+  // const typeArr = ['iphone', 'android', 'ipad', 'windows', 'macos', 'windows phone', 'symbianOS', 'ipod']
+  const mobileTypeArr = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"]
+  const findResult = mobileTypeArr.find(ele => ua.match(new RegExp(ele, 'i')))
+  return findResult || 'pc'
 }
 
 app.get('/getImg', (req, res) => {
   const userAgent = req.headers['user-agent']
   const equipmentType = equipmentTypeDetection(userAgent)
-  const newInfoArr = imgArrInfo.randomImg.reduce((acc, { imgUrl, redirect, chance }, index, arr) => {
-    if (index > 0) {
-      return [...acc, { imgUrl, redirect, chance: acc[index - 1].chance + chance }]
-    }
-    return acc
-  }, [(imgArrInfo.randomImg)[0]])
-  const randomNum = Math.ceil(Math.random() * 100)
-  const result = newInfoArr.find(ele => ele.chance > randomNum)
-  const showImgRenderStr = ejs.render(template.showImg, { params: result.imgUrl === undefined ? [] : [result.imgUrl] }).replace(/\n/g, '')
+  // const newInfoArr = imgArrInfo.randomImg.reduce((acc, { imgUrl, redirect, chance }, index, arr) => {
+  //   if (index > 0) {
+  //     return [...acc, { imgUrl, redirect, chance: acc[index - 1].chance + chance }]
+  //   }
+  //   return acc
+  // }, [(imgArrInfo.randomImg)[0]])
+  // const randomNum = Math.ceil(Math.random() * 100)
+  // const result = newInfoArr.find(ele => ele.chance > randomNum)
+  // const showImgRenderStr = ejs.render(template.showImg, { params: result.imgUrl === undefined ? [] : [result.imgUrl] }).replace(/\n/g, '')
+  const isPc = equipmentType === 'pc'
+  let showImgRenderStr = `<a target="_blank" href="${
+    imgArrInfo.localAddress + imgArrInfo[isPc ? 'pcImg' : 'mobileImg'].wrapUrl
+    }"><img src="${
+    imgArrInfo.localAddress + imgArrInfo[isPc ? 'pcImg' : 'mobileImg'].url
+    }"/></a>`
   const hiddenImgRenderStr = ejs.render(template.hiddenImg, { params: imgArrInfo.mobileHref || [] }).replace(/\n/g, '')
   let scriptContent = `
   var advertContainer = document.createElement('div')
   advertContainer.setAttribute("class", "cccAdvertContainer")
   advertContainer.innerHTML = '${
-    showImgRenderStr + (['android', 'iphone'].includes(equipmentType) ? hiddenImgRenderStr : "")
+    showImgRenderStr + (isPc ? "" : hiddenImgRenderStr)
     }'
   document.getElementsByTagName('body')[0].appendChild(advertContainer);
   `
-  // if (imgArrInfo.mobileHref) {
-  //   imgArrInfo.mobileHref.map(ele => {
-
-  //   })
-  // }
   res.send(scriptContent)
 })
 
